c9c701fb7df665a1cbfa75ea4f76f7b7dbba6aa1
who asks? <user@localhost>
2024-09-10 03:54:31 -0600

    docker and docker-compose for testnet

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,14 @@
-# Use a lightweight Python image
 FROM python:3.12-slim
 
-# Set the working directory
 WORKDIR /app
 
-# Copy the Python script and configuration files
-COPY prometheus_exporter.py config.yaml /app/
+COPY monerod_stats_exporter.py /app/
+COPY docker-compose-config.yml /app/config.yml
 
-# Install required Python libraries
 RUN pip install prometheus-client requests pyyaml
 
-# Expose the default Prometheus exporter port
+VOLUME [ "/data", "/app" ]
 EXPOSE 9123
 
-# Run the Prometheus exporter
-CMD ["python", "prometheus_exporter.py"]
+CMD ["python", "-u", "monerod_stats_exporter.py"]
 
--- monerod_stats_exporter.py
+++ monerod_stats_exporter.py
@@ -9,15 +9,51 @@ from prometheus_client import (
     start_wsgi_server,
     CollectorRegistry,
     Gauge,
+    Info,
     Summary
 )
 
 
 update_time = Summary('monero_stats_update_time', 'Total time to update monero stats')
+DEFAULT_INFO = [
+    'adjusted_time',
+    'alt_blocks_count',
+    'block_size_limit',
+    'block_size_median',
+    'block_weight_limit',
+    'block_weight_median',
+    'busy_syncing',
+    'database_size',
+    'difficulty',
+    'height',
+    'height_without_bootstrap',
+    'nettype',
+    'offline',
+    'status',
+    'synchronized',
+    'target_height',
+    'tx_pool_size',
+    'untrusted',
+    'update_available',
+    'version',
+    'was_bootstrap_ever_used'
+]
+
+DEFAULT_HEADER_INFO = [
+    'block_size',
+    'block_weight',
+    'difficulty',
+    'height',
+    'long_term_weight',
+    'num_txes',
+    'orphan_status',
+    'reward'
+]
 
 
 class MonerodStatsExporter(Thread):
     gauges: dict[str, Gauge] = {}
+    infos: dict[str, Info] = {}
     registry: CollectorRegistry = CollectorRegistry()
     server: WSGIServer = None
     server_thread: Thread = None
@@ -31,9 +67,10 @@ class MonerodStatsExporter(Thread):
 
     def load_config(self) -> dict:
         with open(self.config_file, 'r') as file:
+            print(f'load config {self.config_file}...')
             return load_yaml(file)
 
-    def init_gauges(self):
+    def init(self):
         self.registry.register(update_time)
         for instance in self.config['instances']:
             prefix = f"monero_{instance['network']}_{'pruned_' if instance['pruned'] else ''}"
@@ -59,10 +96,26 @@ class MonerodStatsExporter(Thread):
                 f"Timestamp of the latest block in {instance['network']}",
                 registry=self.registry
             )
+            self.gauges[prefix + 'block_size'] = Gauge(
+                f'{prefix}block_size',
+                f"Block size of latest block in {instance['network']}",
+                registry=self.registry
+            )
+            self.infos[prefix + 'block_header'] = Info(
+                f'{prefix}block_header',
+                f"Block header of latest block in {instance['network']}",
+                registry=self.registry
+            )
+            self.infos[prefix] = Info(
+                prefix[:-1],
+                f"General info of {instance['network']}",
+                registry=self.registry
+            )
 
     def get_file_metrics(self, file_path: str) -> tuple:
         if exists(file_path):
             return getsize(file_path), getmtime(file_path)
+        print(f"path don't exists: {file_path}")
         return None, None
 
     def get_monero_metrics(self, monero_rpc_url: str) -> tuple:
@@ -82,15 +135,39 @@ class MonerodStatsExporter(Thread):
                 result = response.json()['result']['block_header']
                 block_height = result['height']
                 block_timestamp = result['timestamp']
-                return block_height, block_timestamp
+                block_header_info = {key: str(value) for key, value in result.items() if key in DEFAULT_HEADER_INFO}
+                return block_height, block_timestamp, block_header_info
             else:
-                return None, None
+                return None, None, None
         except Exception as e:
             print(f"Error fetching Monero metrics: {e}")
-            return None, None
+            return None, None, None
+
+    def get_monero_info(self, monero_rpc_url: str) -> dict:
+        try:
+            response = post(
+                monero_rpc_url,
+                json={
+                    'jsonrpc':'2.0',
+                    'id':'0',
+                    'method': 'get_info'
+                },
+                headers={
+                    'Content-Type': 'application/json'
+                }
+            )
+            if response.status_code == 200:
+                result = response.json()['result']
+                return {key: str(value) for key, value in result.items() if key in DEFAULT_INFO}
+            else:
+                return {}
+        except Exception as e:
+            print(f"Error fetching Monero info: {e}")
+            return {}
 
     @update_time.time()
     def update_metrics(self) -> None:
+        print('collect metrics...')
         for instance in self.config['instances']:
             prefix = f"monero_{instance['network']}_{'pruned_' if instance['pruned'] else ''}"
 
@@ -99,10 +176,18 @@ class MonerodStatsExporter(Thread):
                 self.gauges[prefix + 'file_size'].set(file_size)
                 self.gauges[prefix + 'file_last_update'].set(last_update)
 
-            block_height, block_timestamp = self.get_monero_metrics(instance['monero_rpc_url'])
+            block_height, block_timestamp, block_header_info = self.get_monero_metrics(instance['monero_rpc_url'])
             if block_height and block_timestamp:
                 self.gauges[prefix + 'block_height'].set(block_height)
                 self.gauges[prefix + 'block_timestamp'].set(block_timestamp)
+            if block_header_info:
+                self.infos[prefix + 'block_header'].info(block_header_info)
+                if 'block_size' in block_header_info:
+                    self.gauges[prefix + 'block_size'].set(float(block_header_info['block_size']))
+        print('collect info...')
+        for instance in self.config['instances']:
+            prefix = f"monero_{instance['network']}_{'pruned_' if instance['pruned'] else ''}"
+            self.infos[prefix].info(self.get_monero_info(instance['monero_rpc_url']))
 
     def start_endpoint(self) -> None:
         port = int(self.config['port']) if 'port' in self.config else 9123
@@ -112,7 +197,7 @@ class MonerodStatsExporter(Thread):
             address,
             self.registry
         )
-        self.init_gauges()
+        self.init()
         print(f'monerod stats exporter listening on port {address}:{port}')
         self.start()
 
@@ -138,6 +223,7 @@ class MonerodStatsExporter(Thread):
 
 
 if __name__ == '__main__':
+    print('Start monerod stats exporter...')
     exporter = MonerodStatsExporter('config.yml')
     def handle_signal(signum, frame) -> None:
         print(f'received signal {signum}, shutdown...')
